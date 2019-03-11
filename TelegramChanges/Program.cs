080e94560524a5bc35e94ed1710026e30974a656
UltWolf <ultwolf@gmail.com>
2019-03-11 14:34:02 +0200

    Add command Unban, rename command

--- TelegramChanges/Program.cs
+++ TelegramChanges/Program.cs
@@ -8,7 +8,7 @@ using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
- 
+
 
 namespace TelegramChanges
 {
@@ -16,83 +16,98 @@ namespace TelegramChanges
     {
         private static readonly TelegramBotClient Bot = new TelegramBotClient("742443229:AAEHNs7QYoLI9Aw463v72HbxrVmiKOgBwE8");
         private static List<Telegram.Bot.Types.Message> _messages = new List<Telegram.Bot.Types.Message>();
+        private static List<ChatMember> _blackListMember = new List<ChatMember>();
         static void Main(string[] args)
         {
             var me = Bot.GetMeAsync().Result;
-            Console.Title = me.Username; 
+            Console.Title = me.Username;
             Bot.OnMessageEdited += BotOnMessageEdit;
-            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;  
-            Bot.OnReceiveError += BotOnReceiveError; 
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+            Bot.OnReceiveError += BotOnReceiveError;
             Bot.OnMessage += BotOnMessageReseived;
-            Bot.StartReceiving(Array.Empty<UpdateType>()); 
+            Bot.StartReceiving(Array.Empty<UpdateType>());
             Console.WriteLine($"Start listening for @{me.Username}");
             Console.ReadLine();
             Bot.StopReceiving();
         }
-         
+
         private static async void BotOnMessageReseived(object sender, MessageEventArgs messageEventArgs)
         {
             var message = messageEventArgs.Message;
-             
-            _messages.Add(message);
-            if (_messages.Count > 2)
+            if (message.Type == MessageType.Text && message.Text !=null)
             {
-                if ((message.Text == _messages[_messages.Count - 1].Text) && (message.Text == _messages[_messages.Count - 2].Text))
+                _messages.Add(message);
+                if (_messages.Count > 2)
                 {
-                    if ((message.From.Id == _messages[_messages.Count - 1].From.Id) && (message.From.Id == _messages[_messages.Count - 2].From.Id))
+                    if ((message.Text == _messages[_messages.Count - 1].Text) && (message.Text == _messages[_messages.Count - 2].Text))
                     {
+                        if ((message.From.Id == _messages[_messages.Count - 1].From.Id) && (message.From.Id == _messages[_messages.Count - 2].From.Id))
+                        {
+                            try
+                            {
+                                
+                                await Bot.SendPhotoAsync(message.Chat, photo: "https://i.ytimg.com/vi/8oKG9KV9NG8/hqdefault.jpg", caption: "Сегодня мы прощаемся с рабом Божьми : " + message.From.FirstName);
+                                _blackListMember.Add(await Bot.GetChatMemberAsync(message.Chat, message.From.Id));
+                                int banSeconds = 10;
+                                await Bot.KickChatMemberAsync(message.Chat.Id, message.From.Id, untilDate: DateTime.UtcNow.AddSeconds(banSeconds));
+
+
+                            }
+                            catch (Exception ex)
+                            {
+                                await Bot.SendTextMessageAsync(message.Chat.Id, "I can kick admins from channel, or kick users from private channel.");
+                            }
+                            return;
+                        }
+                    }
+                }
+
+                switch (message.Text.Split(' ').First())
+                {
+                    case "/work": 
+                        Message essage = await Bot.SendPhotoAsync(
+    chatId: message.Chat.Id,
+    photo: "https://i.ytimg.com/vi/ZIozHiT1ehs/hqdefault.jpg",
+    caption: "Опять работа",
+    parseMode: ParseMode.Html
+    );
+                        break;
+                    case "/unban":
+                        Chat chat = new Chat();
                         try
                         {
-                            int banSeconds = 10;
-                            await Bot.KickChatMemberAsync(message.Chat.Id, message.From.Id, untilDate: DateTime.UtcNow.AddSeconds(banSeconds));
-                           
-
+                            int userId = _blackListMember.Find(m => m.User.Username == message.Text.Split(' ')[1]).User.Id;
+                            await Bot.UnbanChatMemberAsync(message.Chat, userId);
                         }
                         catch(Exception ex)
                         {
-                            await Bot.SendTextMessageAsync(message.Chat.Id,"I can kick admins from channel, or kick users from private channel.");
+                            await Bot.SendTextMessageAsync(message.Chat, "К сожалению мы не можем найти, или убрать со списка данного гражданина.");
                         }
-                        return;
-                    }
+                        break;
+
                 }
             }
-           
-            switch (message.Text.Split(' ').First())
-            {
-                case "/Работать":
-                case "/Работа":
-                case "/За_работу":
-                    Message essage = await Bot.SendPhotoAsync(
-chatId: message.Chat.Id,
-photo: "https://i.ytimg.com/vi/ZIozHiT1ehs/hqdefault.jpg",
-caption: "Опять работа",
-parseMode: ParseMode.Html
-);
-                    break;
-               
-            }
-            
         }
         private static async void BotOnMessageEdit(object sender, MessageEventArgs messageEventArgs)
         {
             var message = messageEventArgs.Message;
 
-            if (message == null || message.Type != MessageType.Text) return; 
+            if (message == null || message.Type != MessageType.Text) return;
             Message essage = await Bot.SendPhotoAsync(
   chatId: message.Chat.Id,
   photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Yvbr0zgZk8-OxIAshDvSnD_SB53a7-F_ywreqThBd67eIdVo6g",
   caption: "Пане президенте, хтось знову змінив повідомлення!",
   parseMode: ParseMode.Html
-); 
+);
 
             foreach (var m in _messages)
             {
-                if(m.MessageId == message.MessageId)
+                if (m.MessageId == message.MessageId)
                 {
-                     
-                        await Bot.SendTextMessageAsync(message.Chat.Id, "From:\n " + m.Text + "\nTo:\n " + message.Text);
-                     
-                    }
+
+                    await Bot.SendTextMessageAsync(message.Chat.Id, "From:\n " + m.Text + "\nTo:\n " + message.Text);
+
+                }
             }
         }
 
@@ -109,7 +124,7 @@ parseMode: ParseMode.Html
                 callbackQuery.Message.Chat.Id,
                 $"Received {callbackQuery.Data}");
         }
-        
+
         private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
         {
             Console.WriteLine("Received error: {0} — {1}",
